30101cbfa85588f31a03ef86d25e46b1757a8a20
Vaibhav Saxena <vaibhav@Vaibhavs-MacBook-Pro.local>
2024-03-24 19:28:46 +0530

    Addind dictionaries

--- dictionary/conditional_filter.py
+++ dictionary/conditional_filter.py
@@ -1,3 +1,5 @@
 def conditional_filter(dict1, condition):
 
-    return { k: v for k,v in dict1}
+    return { k: v for k, v in dict1.items() if condition(k,v)}
+
+print(conditional_filter({'a': 1, 'b': 2, 'c': 3, 'd': 4} , lambda k, v: v % 2 == 0))
---
+++ dictionary/same_frequency.py
@@ -0,0 +1,5 @@
+def same_frequency(list1, list2):
+
+    return (list1.sort()==list2.sort())
+
+print(same_frequency([1, 2, 3, 2, 1],[3, 1, 2, 1, 3]))
